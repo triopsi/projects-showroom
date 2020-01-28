626cbd9a6497c5861c2ead01f113cb072ccef6c6
triopsi <daniel@triopsi.com>
2020-01-28 15:03:27 +0100

    Inti Add

---
+++ projects-showroom.php
@@ -0,0 +1,49 @@
+<?php
+/**
+* Plugin Name: Projects Showroom
+* Plugin URI: https://www.wiki.profoxi.de
+* Description: A very simple showroom for your projects. Create projects and copy-paste the shortcode everywhere in your post or site.
+* Version: 0.0.1
+* Author: triopsi
+* Author URI: http://wiki.profoxi.de
+* Text Domain: psr
+* Domain Path: /lang/
+* License: GPL3
+* License URI: https://www.gnu.org/licenses/gpl-3.0
+*
+* uebns is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* any later version.
+*  
+* uebns is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*  
+* You should have received a copy of the GNU General Public License
+* along with uebns. If not, see https://www.gnu.org/licenses/gpl-3.0.
+**/
+
+//Definie plugin version
+if (!defined('UEBNS_VERSION'))
+    define('UEBNS_VERSION', '0.0.1');
+
+/**
+ * Define path
+ */
+define('PSR_PATH', plugin_dir_path(__FILE__));
+
+/* General */
+/* Loads plugin's text domain. */
+add_action( 'init', 'psr_load_plugin_textdomain' );
+
+
+/**
+ * Init Script. Load languages
+ *
+ * @return void
+ */
+function psr_load_plugin_textdomain() {
+    load_plugin_textdomain( 'psr', FALSE, basename(PSR_PATH.'/lang/') );
+  }
---
+++ readme.txt
@@ -0,0 +1,59 @@
+=== About Us Team ===
+Contributors: triopsi
+Donate link: https://www.paypal.me/triopsi
+Tags: shortcode, site, post, showroom, projects, project
+Requires at least: 5.3
+Tested up to: 5.3.2
+Requires PHP: 5.6
+Stable tag: 0.0.1
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0
+ 
+A very simple showroom for your projects. Very useful and very ease to install.
+ 
+== Description ==
+
+The "projects-showroom" Plugin is a very easy way to show your projects in a showwroom. You create a shortcode and copy-paste the shortcode in your
+post or site. 
+
+= Create features =
+ 
+*   Create and manage projects
+*   filter the categories
+*   add informattion in the projetcbox
+
+= Bugs, technical hints or contribute =
+
+Please give me feedback, contribute and file technical bugs on [GitHub Repository](https://github.com/triopsi/projects-showroom). Thanks :)
+
+= Credits =
+
+Developed by [Triopsi](https://wiki.profoxi.de)
+
+== Installation ==
+ 
+1. Upload `projects-showroom` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+ 
+== Frequently Asked Questions ==
+ 
+Nothing yet.
+ 
+== Screenshots ==
+ 
+1. Projects Showroom on site default layout
+4. Projects Settings
+ 
+== Changelog ==
+ 
+= 0.0.1 =
+* First release.
+ 
+== Upgrade Notice ==
+
+Nothing.
+
+== Donations ==
+
+If you like the plugin, consider a donation to support further development. [Click here](https://www.paypal.me/triopsi)
+ 
